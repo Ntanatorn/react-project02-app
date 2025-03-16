c6ce03a45613aa0636c751c6d0e63def7a0bf723
Ntanatorn <torkaitai0404@gmail.com>
2025-03-16 16:22:59 +0700

    The end Ep.2

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import ShowHello from "./ShowHello"
+import ShowHi from "./ShowHi"
+import ShowHey from "./ShowHey"
+
+function App() {
+  return (
+    <>
+    <ShowHey/>
+      <ShowHello/>
+      <ShowHi/>
+      <ShowHello/>
+      <ShowHey/>
+    </>
+  )
+}
+
+export default App
---
+++ src/ShowHey.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ShowHi from './ShowHi'
+
+function ShowHey() {
+  return (
+    <>
+    <ShowHi/>
+    <h1 style={{color:"red",textAlign:"center"}}>Hey Hey Hey</h1>
+    <ShowHi/>
+    </>
+  )
+}
+
+export default ShowHey
